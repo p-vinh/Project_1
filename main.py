05f2cd2f930120d9d256f302935db1313c1e72dc
random-debug <85326677+p-vinh@users.noreply.github.com>
2023-10-02 10:20:53 -0700

    Setup

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+Recognize Python decimal integer literals (section 2.4.4 - 2.4.5)
+For simplicity, you can assume the input string s length <=20.
+Your program should ask for user input as a string, and check if it can be a valid decimal integer or not.
+You also need to include your NFA design (with unlimited digits allowed
+you can break this into a few smaller NFA's so it's not one humongous machine) drawn using JFlapLinks to an external site.. (Please submit the .jff files.)
+
+
+
+Expand your program to recognize Python octal and hexadecimal integer literals (section 2.4.4 - 2.4.5)
+For simplicity, you can assume the input string s length <=20.
+Your program should ask for user input as a string, and check if it can be a valid decimal integer or not.
+You also need to include your NFA design (with unlimited digits allowed
+you can break this into a few smaller NFA's so it's not one humongous machine) drawn using JFLAP.
+
+"""
+
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
