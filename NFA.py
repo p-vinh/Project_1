140df59683f93b7fd568699fae31aa47256aaf21
random-debug <85326677+p-vinh@users.noreply.github.com>
2023-11-18 13:06:42 -0800

    Created main function. Updated NFA class

--- NFA.py
+++ NFA.py
@@ -4,12 +4,22 @@ class NFA:
         self.states = states
         self.alphabet = alphabet
         self.transitions = transitions
+        self.initial_state = initial_state
         self.current_states = {initial_state}
         self.accepting_states = accepting_states
 
     def process_input(self, input_string):
+        
+        for char in input_string:
+            if char not in self.alphabet:
+                return False
+            
         for symbol in input_string:
             self.current_states = self.get_next_states(symbol)
+            
+            # if there are no next states, return False
+            if not self.current_states:
+                return False
 
         return any(state in self.accepting_states for state in self.current_states)
 
@@ -18,12 +28,20 @@ class NFA:
         for state in self.current_states:
             if (state, symbol) in self.transitions:
                 next_states.update(self.transitions[(state, symbol)])
+            if (state, None) in self.transitions:
+                next_states.update(self.transitions[(state, None)])
+
         return next_states
     
-states = {'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10', 'q11'}
-alphabet = set("0123456789_")
-transitions = {('q0', '0'): {'q1'}, ('q1', '1'): {'q2'}, ('q2', '0'): {'q0'}}
-initial_state = 'q0'
-accepting_states = {'q2'}
+    def reset(self):
+        self.current_states = {self.initial_state}
+    
+# states = {'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10', 'q11'}
+# alphabet = set("0123456789_")
+# transitions = {('q0', '0'): {'q1'}, ('q1', '1'): {'q2'}, ('q2', '0'): {'q0'}}
+# initial_state = 'q0'
+# accepting_states = {'q2'}
 
-nfa = NFA(states, alphabet, transitions, initial_state, accepting_states)
+
+
+# nfa = NFA(states, alphabet, transitions, initial_state, accepting_states)
--- main.py
+++ main.py
@@ -15,6 +15,8 @@ you can break this into a few smaller NFA's so it's not one humongous machine) d
 
 """
 
+import NFA
+
 
 
 def isOperator(s):
@@ -44,7 +46,50 @@ def match(s):
 
 
 def main():
-    pass
+    states = {'q0', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q10', 'q11'}
+    alphabet = set("0123456789_")
+    transitions = {
+                   # non-zero digit 
+                   **{('q0', str(digit)): {'q10'} for digit in range(1, 10)},
+                   **{('q10', str(digit)): {'q10'} for digit in range(1, 10)},
+                   
+                   # lambda transition
+                   ('q10', None) : {'q2', 'q3', 'q8'},
+                   ('q10', '_') : {'q8'},
+                   
+                   # 0+
+                   ('q2', '0') : {'q4'},
+                   ('q4', '0') : {'q4'},
+                   
+                   # (["_"] 0)*
+                   ('q4', None) : {'q5', 'q10'},
+                   ('q5', '_') : {'q6'},
+                   ('q5', None) : {'q6'},
+                   ('q6', '0') : {'q7'},
+                   ('q7', None) : {'q5', 'q10'},
+                   
+                   # (["_"] digit)*
+                   **{('q8', str(digit)): {'q11'} for digit in range(0, 10)},
+                   **{('q11', str(digit)): {'q11'} for digit in range(0, 10)},               
+                   ('q11', None) : {'q3', 'q10'}
+                  }
+    
+    initial_state = 'q0'
+    accepting_states = {'q3'}
+    nfa = NFA.NFA(states, alphabet, transitions, initial_state, accepting_states)
+    
+    while True:
+        s = input("Enter a string: ")
+        
+        if s == "quit":
+            break
+        
+        if nfa.process_input(s):
+            print("Valid decimal integer literal")
+        else:
+            print("Invalid decimal integer literal")
+
+        nfa.reset()    
 
 
 
