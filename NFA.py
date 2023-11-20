11c08538512b1d3395a710e72ec17c5d0cfeb24b
random-debug <85326677+p-vinh@users.noreply.github.com>
2023-11-19 16:56:15 -0800

    Updated Images

--- NFA.py
+++ NFA.py
@@ -1,5 +1,4 @@
 class NFA:
-    #this is decIntegers only
     def __init__(self, states, alphabet, transitions, initial_state, accepting_states):
         self.states = states
         self.alphabet = alphabet
@@ -16,14 +15,11 @@ class NFA:
     
         for symbol in input_string:
             self.current_states = self.get_next_states(symbol)
-            self.current_states.update(self.get_next_states(None)) 
+
             
             # if there are no next states, return False
             if not self.current_states:
                 return False
-        
-        # Edge case: if there is only one character, get the next states.
-        self.current_states = self.get_next_states(None) # Get all lambda transitions from the current states.
         
         return any(state in self.accepting_states for state in self.current_states)
 
@@ -32,20 +28,9 @@ class NFA:
         for state in self.current_states:
             if (state, symbol) in self.transitions:
                 next_states.update(self.transitions[(state, symbol)])
-            if (state, None) in self.transitions:
-                next_states.update(self.transitions[(state, None)])
                 
         return next_states
     
     def reset(self):
         self.current_states = {self.initial_state}
     
-# states = {'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10', 'q11'}
-# alphabet = set("0123456789_")
-# transitions = {('q0', '0'): {'q1'}, ('q1', '1'): {'q2'}, ('q2', '0'): {'q0'}}
-# initial_state = 'q0'
-# accepting_states = {'q2'}
-
-
-
-# nfa = NFA(states, alphabet, transitions, initial_state, accepting_states)
